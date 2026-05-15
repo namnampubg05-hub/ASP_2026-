09611698cd97eca6a3aaa11e146f2444471ccce3
Nam <namd5132@gmail.com>
2026-05-15 16:18:13 +0700

    first commit

---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.Data.Entities
+{
+    // Lớp User đại diện cho người dùng hệ thống (quản trị viên hoặc biên tập viên)
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+    }
+}
